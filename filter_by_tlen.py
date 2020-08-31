a6c89b3f2e5340c478855381dd76ee2565bfb1e5
Kwee Boon Brandon Seah <kb.seah@tuebingen.mpg.de>
2020-08-31 10:57:36 +0200

    Phaseogram function

--- filter_by_tlen.py
+++ filter_by_tlen.py
@@ -1,17 +1,18 @@
 #!/usr/bin/env python
 
-import re
+# import re
 import pysam
 from collections import defaultdict
 import argparse
+import json
 
 parser = argparse.ArgumentParser()
-parser.add_argument("-i","--input",type=str,help="Input BAM file")
-parser.add_argument("-o","--output",type=str,help="Output file",default="filter_out.bam")
-parser.add_argument("--scaffold",type=str,help="Scaffold to filter")
-parser.add_argument("--min_tlen",type=int,default=126,help="Minimum template insert length for read pair")
-parser.add_argument("--max_tlen",type=int,default=166,help="Maximum template insert length for read pair")
-parser.add_argument("--max_mismatch",type=int,default=1,help="Maximum mismatch allowed per alignment")
+parser.add_argument("-i", "--input", type=str, help="Input BAM file")
+parser.add_argument("-o", "--output", type=str, help="Output file", default="filter_out.bam")
+parser.add_argument("--scaffold", type=str, help="Scaffold to filter")
+parser.add_argument("--min_tlen", type=int, default=126, help="Minimum template insert length for read pair")
+parser.add_argument("--max_tlen", type=int, default=166, help="Maximum template insert length for read pair")
+parser.add_argument("--max_mismatch", type=int, default=1, help="Maximum mismatch allowed per alignment")
 args = parser.parse_args()
 
 def get_midpoint(segment):
@@ -72,6 +73,35 @@ def listtuples_to_tsv(inlist, filename):
             fh.write(outstr)
             fh.write("\n")
 
+def fragstarts_dict_to_phaseogram(indict):
+    """
+    Convert dictionary of fragment start positions to phaseogram
+
+    Parameters
+    ----------
+    indict : defaultdict
+        Dict keyed by scaffolds -> pos -> frag start counts
+
+    Returns
+    -------
+    list
+        List of tuples (displacement, count)
+    """
+
+    # TODO: Normalize per scaffold
+    # TODO: Average counts over total read start sites
+
+    phaseogram = defaultdict(int)
+
+    for scaffold in indict:
+        for pos in indict[scaffold]:
+            for i in range(1000):
+                jumppos = i + 1 + int(pos)
+                if jumppos in indict[scaffold].keys():
+                    phaseogram[i+1] += indict[scaffold][jumppos]
+
+    return(phaseogram)
+
 #-------------------------------------------------------------------------------
 
 # Open BAM files
@@ -83,7 +113,11 @@ if args.scaffold:
 else:
     samiter = samfile.fetch()
 
-fragstarts = []
+# Dict of scaffold -> pos -> frag start counts
+fragstarts_fwd = defaultdict(lambda: defaultdict(int))
+fragstarts_rev = defaultdict(lambda: defaultdict(int))
+
+# List of tuples of mipdoints
 midpoints = []
 
 # for each aligned segment, report only those segments with template lengths
@@ -91,13 +125,33 @@ midpoints = []
 for read in samiter:
     if not read.is_secondary and not read.is_duplicate and read.get_tag("NM") <= 1:
         # TODO: Filter out reads with more than one mismatch, with NM tag
-        # Actually mismatches should be summed for each read PAIR, not just each read
-        fragstarts.append(get_frag_start(read))
+        # Actually mismatches should be summed for each read PAIR, not just
+        # each read
+
+        # Count number of fragment starts per base position per scaffold
+        ref,pos,ori = get_frag_start(read)
+        if ori == "fwd":
+            fragstarts_fwd[ref][pos] += 1
+        elif ori == "rev":
+            fragstarts_rev[ref][pos] += 1
+
         if read.template_length >= args.min_tlen and read.template_length <= args.max_tlen:
+            # Record midpoints only for reads within fragment length criteria
             # samout.write(read)
             midpoints.append(get_midpoint(read))
 
 samfile.close
 
-listtuples_to_tsv(fragstarts,"test_fragstarts.tsv")
+# listtuples_to_tsv(fragstarts,"test_fragstarts.tsv")
+phaseogram_fwd = fragstarts_dict_to_phaseogram(fragstarts_fwd)
+# phaseogram_rev = fragstarts_dict_to_phaseogram(fragstarts_rev)
+with open("test_fragstarts_fwd.json", "w") as fh:
+    json.dump(fragstarts_fwd, fh, indent=4)
+with open("test_fragstarts_rev.json", "w") as fh:
+    json.dump(fragstarts_rev, fh, indent=4)
+with open("test_phaseogram_fwd.json", "w") as fh:
+    json.dump(phaseogram_fwd, fh, indent=4)
+# with open("test_phaseogram_rev.json", "w") as fh:
+#     json.dump(phaseogram_rev, fh, indent=4)
 listtuples_to_tsv(midpoints,"test_midpoints.tsv")
+
